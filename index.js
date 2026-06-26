4de5d54229ccaebed91201c8c86c58e2e7501452
Navied-dipu <navieddipu08@gmail.com>
2026-06-26 21:42:39 +0600

    add post and get api

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const express = require('express');
-const app = express()
-const port = 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const db = client.db("wooaiDB");
+  const promptsCollection = db.collection("prompts");
+  try {
+    app.get("/api/prompts", async (req, res) => {
+      const prompts = await promptsCollection.find().toArray();
+      res.send(prompts);
+    });
+    app.post("/api/prompts", async (req, res) => {
+      const prompt = req.body;
+      const result = await promptsCollection.insertOne({ prompt });
+      res.send(result);
+    });
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
